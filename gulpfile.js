b046957bdd808e5856ad3612bc4fdb8add08200d
unknown <sacjain1@WKWIN0707075.global.publicisgroupe.net>
2018-07-09 16:45:46 +0530

    complete npm installation

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,35 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browser = require('browser-sync').create();
+var browserSync = require('browser-sync').create();
 
-// gulp.task('sass', function () {
-//     return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
-//         .pipe(sass())
-//         .pipe(gulp.dest('app/css'))
-//         .pipe(browserSync.reload({
-//             stream: true
-//         }))
-// });
-// gulp.task('browser-sync', function () {
-//     browserSync.init(["css/*.css", "js/*.js"], {
-//         server: {
-//             baseDir: "./app"
-//         }
-//     });
-// });
-// gulp.task('watch', ['browserSync', 'sass'], function () {
-//     gulp.watch('app/scss/**/*.scss', ['sass']);
-//     // Reloads the browser whenever HTML or JS files change
-//     gulp.watch('app/*.html', browserSync.reload);
-//     gulp.watch('app/js/**/*.js', browserSync.reload);
-// });
-// The default task (called when you run `gulp` from cli)
-gulp.task("build", function () {
-    console.log("sachin")
+gulp.task('sass', function () {
+    return gulp.src('app/scss/*.scss') // Gets all files ending with .scss in app/scss
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
-gulp.task('default', 'build');
+gulp.task('js', function () {
+    return gulp.src(['node_modules\jquery\dist\jquery.min.js', 'node_modules\bootstrap\dist\js\bootstrap.js', '']) // Gets all files ending with .scss in app/scss
+        .pipe(gulp.dest('app/js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+gulp.task('browser-sync', function () {
+    browserSync.init(["app/css/*.css", "app/js/*.js"], {
+        server: {
+            baseDir: "./app"
+        }
+    });
+});
+gulp.task('watch', ['browser-sync', 'sass'], function () {
+    gulp.watch('app/scss/*.scss', ['sass']);
+    // Reloads the browser whenever HTML or JS files change
+    gulp.watch('app/index.html', browserSync.reload);
+    gulp.watch('app/js/*.js', browserSync.reload);
+});
+//The default task (called when you run `gulp` from cli)
+
+gulp.task('default', ['watch']);
